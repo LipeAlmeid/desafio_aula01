a3e022013aaf7a0ae3ba914f1029e99dbde644cd
Felipe Almeida <felipealmeida37@yahoo.com.br>
2024-06-25 21:02:06 -0300

    realizado algumas tratativas de erros

--- main.py
+++ main.py
@@ -4,15 +4,25 @@ CONSTANTE_BONUS = 1000
 
 nome = input("Digite seu nome: ")
 
+if nome.isdigit():
+    print("Voce digitou o seu nome errado")
+    exit()
+elif len(nome) == 0:
+    print("Voce nao digitou nada")
+    exit()
+elif nome.isspace():
+    print("Voce digitou so espaço")
+    exit()
+
 #print(nome)
 
 # 02) Solicita ao usuário que digite o valor do seu salário
 # COnverte a entrada para um número de ponto flutuante
 
 salario = float(input("Digite o seu salário: "))
-
-#print(salario)
-
+if salario.isspace():
+    print("Voce digitou so espaco")
+    exit()
 
 # 03) Solicita ao usuário que digite o valor do bonus recebido 
 # Converte a entrada para um número de ponto flutuante
@@ -33,6 +43,8 @@ print(bonus_final)
 
 # 06) Imprime a mensagem personalizada incluindo o nome do usuário, salário e bônus
 
-print(f"O usuario {usuario} possui o salario `{salario} e o bonus {bonus} com o bonus final {bonus_final}")
+print(f"O usuario {usuario} possui o salario {salario} e o bonus {bonus} com o bonus final {bonus_final}")
 
 # Bônus: Quantos bugs e riscos voce consegue indentificar nesse programa?
+
+
